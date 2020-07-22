24420456225f56943aa0fa85f509e3ed3ae9d1d6
YazanAlmalki <Mr.yazan4393@gmail.com>
2020-07-22 16:12:38 +0300

    Initial commit

--- build/js/script.js
+++ build/js/script.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
 
     $('[data-toggle="tooltip"]').tooltip();
 
+    $('[data-add-to-cart]').click(function(e) {
+        alert('أضيف المُنتج إلى عربة الشراء');
+        e.stopPropagation();
+    });
+
 });
